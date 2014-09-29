bc2eeb15812567dd4b4d76fa532d156885d06a08
Mark Piper <mark.piper@colorado.edu>
2014-09-29 15:38:39 -0600

    Update test_build.py

--- packager/rpm/test/test_build.py
+++ packager/rpm/test/test_build.py
@@ -3,34 +3,17 @@
 from packager.rpm.build import BuildRPM
 from nose.tools import *
 
+model_name = "hydrotrend"
+
 @raises(TypeError)
 def test_fail_with_no_parameters():
-    BuildRPM(None)
+    BuildRPM()
 
-@raises(TypeError)
-def test_fail_with_one_parameter():
-    BuildRPM("hydrotrend")
+def test_model_version_none():
+    BuildRPM(model_name, None, None, None)
 
-def test_hydrotrend_version_none():
-    BuildRPM("hydrotrend", None)
+def test_model_version_head():
+    BuildRPM(model_name, "head", None, None)
 
-def test_hydrotrend_version_head():
-    BuildRPM("hydrotrend", "head")
-
-#def test_hydrotrend_tagged_version():
-#    BuildRPM("hydrotrend", "3.0.2")
-
-def test_cem_version_head():
-    BuildRPM("cem", "head")
-
-#def test_cem_tagged_version():
-#    BuildRPM("cem", "0.2")
-
-def test_child_version_head():
-    BuildRPM("child", "head")
-
-def test_sedflux_version_head():
-    BuildRPM("sedflux", "head")
-
-def test_adi2d_version_head():
-    BuildRPM("adi-2d", "head")
+#def test_model_tagged_version():
+#    BuildRPM(model_name, "3.0.2")
