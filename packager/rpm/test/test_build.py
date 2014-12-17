12fc9ec4f61412fe67825a2aa79dd772d815fedc
Mark Piper <mark.piper@colorado.edu>
2014-12-17 09:51:12 -0700

    Update BuildRPM unit tests

--- packager/rpm/test/test_build.py
+++ packager/rpm/test/test_build.py
@@ -9,11 +9,11 @@ model_name = "hydrotrend"
 def test_fail_with_no_parameters():
     BuildRPM()
 
-def test_model_version_none():
-    BuildRPM(model_name, None, None, None)
+# def test_model_version_none():
+#     BuildRPM(model_name, None, None, None, None)
 
-def test_model_version_head():
-    BuildRPM(model_name, "head", None, None)
+# def test_model_version_head():
+#     BuildRPM(model_name, "head", None, None, None)
 
 #def test_model_tagged_version():
 #    BuildRPM(model_name, "3.0.2")
